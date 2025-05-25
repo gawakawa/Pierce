0be66a401f335fcd89e93feef4a6c48418c5c9ef
gawakawa <mashurakiryu@icloud.com>
2025-05-26 00:05:58 +0900

    :sparkles: Add definitions and theorems

--- Peirce/Basic.lean
+++ Peirce/Basic.lean
@@ -1,1 +1,11 @@
-def hello := "world"
+def LEM : Prop := ∀ P : Prop, P ∨ ¬P
+
+def DN : Prop := ∀ P : Prop, ¬¬P → P
+
+def Peirce : Prop := ∀ P Q : Prop, ((P → Q) → P) → P
+
+theorem lem_implies_dn : LEM → DN := by sorry
+
+theorem dn_implies_peirce : DN → Peirce := by sorry  
+
+theorem peirce_implies_lem : Peirce → LEM := by sorry
